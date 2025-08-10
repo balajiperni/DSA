8ae0dde4ea9bd865d729239f8b796fbcfeee15b3
balajiperni <168921558+balajiperni@users.noreply.github.com>
2025-08-10 12:13:59 +0530

    Time: 166 ms (49.73%) | Memory: 16.4 MB (69.82%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.replace(" ", "")
+        stack = []
+        num = 0
+        sign = '+'
+        for i, ch in enumerate(s):
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            if not ch.isdigit() or i == len(s) - 1:
+                if ch in "+-*/" or i == len(s) - 1:
+                    if sign == '+':
+                        stack.append(num)
+                    elif sign == '-':
+                        stack.append(-num)
+                    elif sign == '*':
+                        stack[-1] = stack[-1] * num
+                    elif sign == '/':
+                        stack[-1] = int(stack[-1] / float(num))
+                    sign = ch
+                    num = 0
+        return sum(stack)
