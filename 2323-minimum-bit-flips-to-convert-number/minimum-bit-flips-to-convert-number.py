698367a6c0d33bd8a8bbca40fa119202a6e2f5a7
balajiperni <168921558+balajiperni@users.noreply.github.com>
2025-08-15 21:41:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (0.00%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def minBitFlips(self, start, goal):
+        """
+        :type start: int
+        :type goal: int
+        :rtype: int
+        """
+        diff = start ^ goal
+        return bin(diff).count("1")
+        
